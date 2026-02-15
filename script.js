4f94883aa7b359211800180f972582200b51d332
Witaya252525 <witaya.c@kkumail.com>
2026-02-15 08:20:09 +0700

    add static css and javasscripts

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function getAllLabels() {
+    const labels  = document.getElementsByTagName("label");
+    return labels;  
+
+}
+
+window.onload = function() {
+    // query for all html label tags and add a click event listener to each one
+    const labels  = getAllLabels();
+  
+};
+
